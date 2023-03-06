da6b8a188a27559c81c252c4e0f830759b876699
louisg1337 <louisgrassi1337@gmail.com>
2023-03-05 23:35:21 -0500

    added button functionality to scroll to components

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.css'
@@ -9,25 +9,30 @@ import Landing from '../../components/Landing'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [current, setCurrent] = useState();
   const welcomeRef = useRef();
   const aboutRef = useRef();
   const workRef = useRef();
   const contactRef = useRef();
 
+  const scrollTo = (ref) => {
+    console.log('helllo')
+    ref.current.scrollIntoView({behavior: "smooth"});
+  }
 
   return (
     <main>
       <div className='pickerContainer'>
-        <div className='choiceContainer'><div className='choice'/></div>
-        <div className='choiceContainer'><div className='choice'/></div>
-        <div className='choiceContainer'><div className='choice'/></div>
-        <div className='choiceContainer'><div className='choice'/></div>
+        <div onClick={() => scrollTo(welcomeRef)} className='choiceContainer'><div className='choice'/></div>
+        <div onClick={() => scrollTo(aboutRef)} className='choiceContainer'><div className='choice'/></div>
+        <div onClick={() => scrollTo(workRef)} className='choiceContainer'><div className='choice'/></div>
+        <div onClick={() => scrollTo(contactRef)} className='choiceContainer'><div className='choice'/></div>
       </div>
       <div className='landingContainer'>
-        <Landing name={"Welcome"}/>
-        <Landing name={"About"}/>
-        <Landing name={"Work"}/>
-        <Landing name={"Contact"}/>
+        <div ref={welcomeRef}><Landing name={"Welcome"}/></div>
+        <div ref={aboutRef}><Landing name={"About"}/></div>
+        <div ref={workRef}><Landing name={"Work"}/></div>
+        <div ref={contactRef}><Landing name={"Contact"}/></div>
       </div>
     </main>
   )
