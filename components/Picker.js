b5aa0280377f7f0d72b5de226ce02bfe90661929
louisg1337 <louisgrassi1337@gmail.com>
2023-03-06 21:43:21 -0500

    Made the tab picker responsive to scroll

--- components/Picker.js
+++ components/Picker.js
@@ -1,19 +1,22 @@
+import { useEffect } from 'react';
 import './picker.css'
 
 export default function Picker({welcomeRef, aboutRef, workRef, contactRef, scrollTo, current}) {
+    const buttonColor = 'white';
+
     return (
         <div className='pickerContainer'>
             <div onClick={() => scrollTo(welcomeRef, "welcome")} className='choiceContainer'>
-            <div style={current == "welcome" ? {backgroundColor: 'white'} : {}} className='choice'/>
+            <div style={current == "welcome" ? {backgroundColor: buttonColor} : {}} className='choice'/>
             </div>
             <div onClick={() => scrollTo(aboutRef, "about")} className='choiceContainer'>
-            <div style={current == "about" ? {backgroundColor: 'white'} : {}} className='choice'/>
+            <div style={current == "about" ? {backgroundColor: buttonColor} : {}} className='choice'/>
             </div>
             <div onClick={() => scrollTo(workRef, "work")} className='choiceContainer'>
-            <div style={current == "work" ? {backgroundColor: 'white'} : {}}className='choice'/>
+            <div style={current == "work" ? {backgroundColor: buttonColor} : {}}className='choice'/>
             </div>
             <div onClick={() => scrollTo(contactRef, "contact")} className='choiceContainer'>
-            <div style={current == "contact" ? {backgroundColor: 'white'} : {}} className='choice'/>
+            <div style={current == "contact" ? {backgroundColor: buttonColor} : {}} className='choice'/>
             </div>
       </div>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.css'
@@ -11,15 +11,38 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [current, setCurrent] = useState("welcome");
+  const [currentPos, setCurrentPos] = useState(0)
+  const queue = ['welcome', 'about', 'work', 'contact'];
+
+  const bodyRef = useRef();
   const welcomeRef = useRef();
   const aboutRef = useRef();
   const workRef = useRef();
   const contactRef = useRef();
 
+
+
   const scrollTo = (ref, curr) => {
-    setCurrent(curr)
+    // setCurrent(curr)
     ref.current.scrollIntoView({behavior: "smooth"});
   }
+
+  const getscroll = () => {
+    const sectionHeight = welcomeRef.current.offsetHeight;
+    const scrollPos = bodyRef.current.scrollTop + sectionHeight;
+    console.log(sectionHeight)
+    console.log(scrollPos)
+
+    if (scrollPos <= currentPos || scrollPos >= currentPos) {
+      // Find next section
+      const nextSection = Math.floor(scrollPos / sectionHeight);
+
+      // Set new vars
+      setCurrent(queue[nextSection - 1]);
+      setCurrentPos(nextSection * sectionHeight);
+
+    } 
+  };
 
   return (
     <main>
@@ -30,7 +53,7 @@ export default function Home() {
               scrollTo={scrollTo}
               current={current}
       />
-      <div className='landingContainer'>
+      <div ref={bodyRef} className='landingContainer' onScroll={getscroll}>
         <div ref={welcomeRef}><Landing name={"Welcome"}/></div>
         <div ref={aboutRef}><Landing name={"About"}/></div>
         <div ref={workRef}><Landing name={"Work"}/></div>
