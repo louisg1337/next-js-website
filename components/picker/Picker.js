2805f4b3843bfe58ab7889cd35f72a98baefd7ec
louisg1337 <louisgrassi1337@gmail.com>
2023-03-27 23:41:31 -0400

    Fixed picker glitch with ref

--- components/picker/Picker.js
+++ components/picker/Picker.js
@@ -1,24 +1,15 @@
 import { motion } from "framer-motion"
+import { forwardRef } from "react"
 import './picker.css'
 
 export default function Picker({welcomeRef, aboutRef, workRef, contactRef, scrollTo, current}) {
-    const data = ["welcome", "about", "work", "contact"]
+    const data = [{item: "welcome", ref: welcomeRef}, {item: "about", ref: aboutRef}, {item: "work", ref: workRef}, {item:"contact", ref: contactRef}]
 
     return (
         <div className='pickerContainer'>
-            {data.map((item, key) => {
-                let ref = null;
-                switch(item){
-                    case "welcome":
-                        ref = welcomeRef;
-                    case "about":
-                        ref = aboutRef;
-                    case "work":
-                        ref = workRef;
-                    case "contact":
-                        ref = contactRef
-                }
-
+            {data.map(({item, ref}, key) => {
+                console.log(item)
+                console.log(ref)
                 return (
                     <Node key={key} name={item} ref={ref} scrollTo={scrollTo} current={current}/>
                 )
@@ -27,10 +18,15 @@ export default function Picker({welcomeRef, aboutRef, workRef, contactRef, scrol
     )
 }
 
-const Node = ({name, ref, scrollTo, current}) => {
+const Node = forwardRef(({name, scrollTo, current}, ref) => {
     return (
         <div onClick={() => scrollTo(ref, name)} className='choiceContainer'>
-            <motion.div animate={{rotate: 90}} style={current == name ? {backgroundColor: '#FF8484', transition: 'background-color 0.4s ease'} : {}} className='choice'/>
+            <motion.div 
+            initial={current != name && {rotate: 45}} 
+            animate={current == name ? {rotate: 90} : {}} 
+            style={current == name ? {backgroundColor: '#FF8484', transition: 'background-color 0.4s ease'} : {}} 
+            className='choice'
+            />
         </div>
     );
-}
+});
